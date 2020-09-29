92156f1cf102ddb75401458d264442df8aa73bec
Hao Zhang <haozhang.ml@hotmail.com>
2020-09-29 09:44:55 +0900

    add 0094 solutions

---
+++ 0094/sol2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void printArray(vector<int> arr) {
+    if (arr.size() == 0) cout << "NULL";
+    else {
+        for (auto a : arr) cout << a << ' ';
+    }
+    cout << endl;
+}
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        if (root != nullptr) {
+            auto ansLeft = inorderTraversal(root->left);
+            ans.insert(ans.end(), ansLeft.begin(), ansLeft.end());
+            ans.push_back(root->val);
+            auto ansRight = inorderTraversal(root->right);
+            ans.insert(ans.end(), ansRight.begin(), ansRight.end());
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    // Example 1
+    cout << "Example 1:" << endl;
+    TreeNode *root = new TreeNode(1);
+    TreeNode *ptr1 = new TreeNode(2);
+    TreeNode *ptr2 = new TreeNode(3);
+    root->right = ptr1;
+    ptr1->left = ptr2;
+
+    // Example 4
+    // cout << "Example 4:" << endl;
+    // TreeNode *root = new TreeNode(1);
+    // TreeNode *ptr1 = new TreeNode(2);
+    // root->left = ptr1;
+
+    // Example 5
+    // cout << "Example 5:" << endl;
+    // TreeNode *root = new TreeNode(1);
+    // TreeNode *ptr1 = new TreeNode(2);
+    // root->right = ptr1;
+
+    // Solution
+    Solution sol;
+    auto ans = sol.inorderTraversal(root);
+    printArray(ans);
+
+    return 0;
+}
---
+++ 0094/sol3.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void printArray(vector<int> arr) {
+    if (arr.size() == 0) cout << "NULL";
+    else {
+        for (auto a : arr) cout << a << ' ';
+    }
+    cout << endl;
+}
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> s;
+        
+        if (root != nullptr) {
+            auto thisNode = root;
+            while (thisNode != nullptr) {
+                s.push(thisNode);
+                thisNode = thisNode->left;
+            }
+            while (!s.empty()) {
+                thisNode = s.top();
+                s.pop();
+                ans.push_back(thisNode->val);
+                thisNode = thisNode->right;
+                while(thisNode != nullptr) {
+                    s.push(thisNode);
+                    thisNode = thisNode->left;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    // Example 1
+    cout << "Example 1:" << endl;
+    TreeNode *root = new TreeNode(1);
+    TreeNode *ptr1 = new TreeNode(2);
+    TreeNode *ptr2 = new TreeNode(3);
+    root->right = ptr1;
+    ptr1->left = ptr2;
+
+    // Example 4
+    // cout << "Example 4:" << endl;
+    // TreeNode *root = new TreeNode(1);
+    // TreeNode *ptr1 = new TreeNode(2);
+    // root->left = ptr1;
+
+    // Example 5
+    // cout << "Example 5:" << endl;
+    // TreeNode *root = new TreeNode(1);
+    // TreeNode *ptr1 = new TreeNode(2);
+    // root->right = ptr1;
+
+    // Solution
+    Solution sol;
+    auto ans = sol.inorderTraversal(root);
+    printArray(ans);
+
+    return 0;
+}
